518e742d9316e58842fc53733e6aace62184f087
sinwoobang <sinwoobang@gmail.com>
2015-11-27 00:34:56 +0900

    Translate main.js in Korean

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ try {
   var pathToken = process.env.SLACK_POKER_BOT_TOKEN;
   var token = pathToken || fs.readFileSync('token.txt', 'utf8').trim();
 } catch (error) {
-  console.log("Your API token should be placed in a 'token.txt' file, which is missing.");
+  console.log("API 토큰이 'token.txt' 파일에 있어야합니다. 파일이 존재하지 않습니다.");
   return;
 }
 
@@ -14,7 +14,7 @@ var Bot = require('./bot');
 var bot = new Bot(token);
 bot.login();
 
-// Heroku requires the process to bind to this port within 60 seconds or it is killed 
+// Heroku requires the process to bind to this port within 60 seconds or it is killed
 http.createServer(function(req, res) {
   res.end('SLACK_POKER_BOT');
 }).listen(process.env.PORT || 5000)
